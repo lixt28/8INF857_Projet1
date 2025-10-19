f4877ab0bd7dbc0938c86ee48a114a956ca573d5
lixt28 <148846244+lixt28@users.noreply.github.com>
2025-10-18 22:55:58 -0400

    Update 05_install_snort3.sh

--- scripts/05_install_snort3.sh
+++ scripts/05_install_snort3.sh
@@ -1,43 +1,28 @@
-sudo apt update
-sudo apt install -y \
-  build-essential cmake pkg-config \
-  libpcap-dev libpcre2-dev libdnet-dev \
-  luajit libluajit-5.1-dev \
-  zlib1g-dev libhwloc-dev liblzma-dev libssl-dev \
-  libhyperscan-dev libflatbuffers-dev libunwind-dev \
-  autoconf automake libtool flex bison
 #!/usr/bin/env bash
-set -e
 # Script d'aide pour compiler Snort3 (adapté de https://docs.snort.org/start/installation)
 # Exécuter avec sudo sur la VM monitoring
-
-sudo apt update
-sudo apt install -y build-essential cmake git libpcap-dev libpcre3-dev libdnet-dev       libdumbnet-dev zlib1g-dev liblzma-dev openssl libssl-dev pkg-config libhwloc-dev       libluajit-5.1-dev libluajit-5.1-common flex bison automake autoconf libtool
+sudo apt install build-essential g++ automake git autoconf libtool libpcap-dev libpcre3-dev libnet1-dev zlib1g-dev luajit hwloc libdnet-dev libdumbnet-dev bison flex liblzma-dev openssl libssl-dev pkg-config libhwloc-dev cmake cpputest libsqlite3-dev uuid-dev libcmocka-dev libnetfilter-queue-dev libmnl-dev autotools-dev libluajit-5.1-dev libunwind-dev libfl-dev
 
 # LibDAQ
-if [ ! -d /tmp/libdaq ]; then
-  git clone https://github.com/snort3/libdaq.git /tmp/libdaq
-fi
-cd /tmp/libdaq || exit 1
-./bootstrap || true
+git clone https://github.com/snort3/libdaq.git /tmp/libdaq
+
+cd libdaq
+./bootstrap
 ./configure --prefix=/usr/local/lib/daq_s3
-make -j"$(nproc)"
+make
 sudo make install
-echo "/usr/local/lib/daq_s3/lib/" | sudo tee /etc/ld.so.conf.d/libdaq3.conf
+cat /etc/ld.so.conf.d/libdaq3.conf
 sudo ldconfig
 
 # Snort3
-if [ ! -d /tmp/snort3 ]; then
-  git clone https://github.com/snort3/snort3.git /tmp/snort3
-fi
-cd /tmp/snort3 || exit 1
-export MY_PATH=/usr/local/snort
+git clone https://github.com/snort3/snort3.git 
+export MY_PATH=/usr/local/
 mkdir -p "$MY_PATH"
-./configure_cmake.sh --prefix="$MY_PATH"        --with-daq-includes=/usr/local/lib/daq_s3/include/        --with-daq-libraries=/usr/local/lib/daq_s3/lib/
+./configure_cmake.sh --prefix="$MY_PATH" --with-daq-includes=/usr/local/lib/daq_s3/include/ --with-daq-libraries=/usr/local/lib/daq_s3/lib/
 
 cd build
 make -j"$(nproc)"
 sudo make install
 
-"$MY_PATH/bin/snort" -V || true
+snort -V
 echo "Snort3 installé dans $MY_PATH. Si 'snort -V' fonctionne, continuez la configuration (snort.lua, rules)."
