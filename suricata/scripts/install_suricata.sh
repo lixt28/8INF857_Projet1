45bfbf1b93b76ef1d2511dc800658e59ea83b0d1
Linajanaan <linajanaansawadogo@gmail.com>
2025-10-19 07:32:40 -0400

    Update install_suricata.sh

--- suricata/scripts/install_suricata.sh
+++ suricata/scripts/install_suricata.sh
@@ -1,23 +1,10 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
-echo "[install] Installation Suricata (ppa: oisf/suricata-stable)"
-
 sudo apt update
-sudo apt install -y software-properties-common curl jq
-
+sudo apt install -y software-properties-common
 sudo add-apt-repository -y ppa:oisf/suricata-stable
 sudo apt update
-
-sudo apt install -y suricata suricata-update
-
-sudo mkdir -p /etc/suricata/rules /var/lib/suricata/rules /var/log/suricata
-sudo chown -R suricata:suricata /var/log/suricata || true
- 
-echo "[install] suricata-update initial (ok si warning non bloquant)"
-sudo suricata-update || true
-
-if [[ -f /var/lib/suricata/rules/suricata.rules ]]; then
-  sudo ln -sf /var/lib/suricata/rules/suricata.rules /etc/suricata/rules/suricata.rules
-fi
+sudo apt install -y suricata jq
+sudo systemctl enable suricata
+sudo systemctl stop suricata || true
