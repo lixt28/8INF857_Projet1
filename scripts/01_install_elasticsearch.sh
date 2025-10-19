75b01be73e7e31b6c9153fe5f2b5a2d1d99ad732
lixt28 <148846244+lixt28@users.noreply.github.com>
2025-10-19 00:53:21 -0400

    Update 01_install_elasticsearch.sh

--- scripts/01_install_elasticsearch.sh
+++ scripts/01_install_elasticsearch.sh
@@ -2,12 +2,8 @@
 sudo apt update
 sudo apt install -y default-jdk
 curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elastic.gpg
-echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
 echo "deb [signed-by=/usr/share/keyrings/elastic.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
 sudo apt update
 sudo apt install -y elasticsearch
-sudo sed -i 's/#cluster.name.*/cluster.name: "lab-cluster"/' /etc/elasticsearch/elasticsearch.yml || true
-sudo sed -i 's/#network.host.*/network.host: 0.0.0.0/' /etc/elasticsearch/elasticsearch.yml || true
-sudo systemctl enable --now elasticsearch
-#sleep 3
-#curl -s 'http://localhost:9200/_cluster/health?pretty'
+sudo systemctl start elasticsearch
+sudo systemctl enable elasticsearch
