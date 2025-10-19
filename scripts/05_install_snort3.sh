998790246b364a1c9b5ee1d8f9ce232795e71bc1
lixt28 <148846244+lixt28@users.noreply.github.com>
2025-10-19 01:48:39 -0400

    Update 05_install_snort3.sh

--- scripts/05_install_snort3.sh
+++ scripts/05_install_snort3.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/snort3/snort3.git 
 export MY_PATH=/usr/local/
 mkdir -p "$MY_PATH"
+cd snort3
 ./configure_cmake.sh --prefix="$MY_PATH" --with-daq-includes=/usr/local/lib/daq_s3/include/ --with-daq-libraries=/usr/local/lib/daq_s3/lib/
 
 cd build
