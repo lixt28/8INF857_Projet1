fe1b93bd04d35622e47f48af531560ce2f2f69d2
lixt28 <148846244+lixt28@users.noreply.github.com>
2025-10-19 01:36:24 -0400

    Update 05_install_libDAQ.sh

--- scripts/05_install_libDAQ.sh
+++ scripts/05_install_libDAQ.sh
@@ -4,8 +4,7 @@
 sudo apt install -y build-essential g++ automake git autoconf libtool libpcap-dev libpcre3-dev libnet1-dev zlib1g-dev luajit hwloc libdnet-dev libdumbnet-dev bison flex liblzma-dev openssl libssl-dev pkg-config libhwloc-dev cmake cpputest libsqlite3-dev uuid-dev libcmocka-dev libnetfilter-queue-dev libmnl-dev autotools-dev libluajit-5.1-dev libunwind-dev libfl-dev
 
 # LibDAQ
-git clone https://github.com/snort3/libdaq.git /tmp/libdaq
-
+git clone https://github.com/snort3/libdaq.git
 cd libdaq
 ./bootstrap
 ./configure --prefix=/usr/local/lib/daq_s3
