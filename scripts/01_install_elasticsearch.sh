416aea3b289e462dc7ee6b52604cb123939fe105
lixt28 <148846244+lixt28@users.noreply.github.com>
2025-10-19 00:23:52 -0400

    Update 01_install_elasticsearch.sh

--- scripts/01_install_elasticsearch.sh
+++ scripts/01_install_elasticsearch.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 sudo apt update
+sudo apt install -y default-jdk
 curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elastic.gpg
 echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
+echo "deb [signed-by=/usr/share/keyrings/elastic.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
 sudo apt update
 sudo apt install -y elasticsearch
 sudo sed -i 's/#cluster.name.*/cluster.name: "lab-cluster"/' /etc/elasticsearch/elasticsearch.yml || true
