d8c55c6c838f93e50f4bcaa9a36173f11dd7772f
lixt28 <148846244+lixt28@users.noreply.github.com>
2025-10-19 14:19:04 -0400

    Update 06_deploy_snort_rules.sh

--- scripts/06_deploy_snort_rules.sh
+++ scripts/06_deploy_snort_rules.sh
@@ -7,6 +7,6 @@ sudo cp "$SNORT_ETC/snort.lua" "$SNORT_ETC/snort.lua.bak_$(date +%Y-%m-%d_%H-%M-
 sudo cp configs/snort/snort.lua "$SNORT_ETC/snort.lua"
 sudo mkdir -p /var/log/snort
 sudo chown root:root /var/log/snort || true
-# echo "Règles déployées. Lance Snort (ex):"
-# echo "sudo /usr/local/snort/bin/snort -c /usr/local/etc/snort/snort.lua -i enp0s3 -A alert_json -l /var/log/snort -k none"
+echo "Règles déployées. Lance Snort (ex):"
+echo "sudo /usr/local/snort/bin/snort -c /usr/local/etc/snort/snort.lua -i enp0s3 -A alert_json -l /var/log/snort -k none"
 
