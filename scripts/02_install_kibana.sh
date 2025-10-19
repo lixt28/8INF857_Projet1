6d444eb03057e2f01f9db6dbb5b9f219d353ca45
lixt28 <148846244+lixt28@users.noreply.github.com>
2025-10-19 01:01:25 -0400

    Update 02_install_kibana.sh

--- scripts/02_install_kibana.sh
+++ scripts/02_install_kibana.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt update
 sudo apt install -y kibana
-sudo sed -i 's/#server.host: "localhost"/server.host: "0.0.0.0"/' /etc/kibana/kibana.yml || true
-sudo sed -i 's/#elasticsearch.hosts: .*/elasticsearch.hosts: ["http:\/\/localhost:9200"]/' /etc/kibana/kibana.yml || true
+#sudo sed -i 's/#server.host: "localhost"/server.host: "0.0.0.0"/' /etc/kibana/kibana.yml || true
+#sudo sed -i 's/#elasticsearch.hosts: .*/elasticsearch.hosts: ["http:\/\/localhost:9200"]/' /etc/kibana/kibana.yml || true
 sudo systemctl enable --now kibana
