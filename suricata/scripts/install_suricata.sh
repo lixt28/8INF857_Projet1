3e47bee4910ae2cc679b9b34e8c0d3e4cd615184
Linajanaan <linajanaansawadogo@gmail.com>
2025-10-19 07:46:24 -0400

    Update install_suricata.sh

--- suricata/scripts/install_suricata.sh
+++ suricata/scripts/install_suricata.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+
 sudo apt update
 sudo apt install -y software-properties-common
 sudo add-apt-repository -y ppa:oisf/suricata-stable
 sudo apt update
 sudo apt install -y suricata jq
+
+
 sudo systemctl enable suricata
 sudo systemctl stop suricata || true
