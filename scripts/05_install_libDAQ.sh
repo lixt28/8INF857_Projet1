4c26873e348865645c238b41d573242f22ebdb48
lixt28 <148846244+lixt28@users.noreply.github.com>
2025-10-19 01:48:04 -0400

    Update 05_install_libDAQ.sh

--- scripts/05_install_libDAQ.sh
+++ scripts/05_install_libDAQ.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Script d'aide pour compiler Snort3 (adapté de https://docs.snort.org/start/installation)
 # Exécuter avec sudo sur la VM monitoring
-sudo apt install -y build-essential g++ automake git autoconf libtool libpcap-dev libpcre3-dev libnet1-dev zlib1g-dev luajit hwloc libdnet-dev libdumbnet-dev bison flex liblzma-dev openssl libssl-dev pkg-config libhwloc-dev cmake cpputest libsqlite3-dev uuid-dev libcmocka-dev libnetfilter-queue-dev libmnl-dev autotools-dev libluajit-5.1-dev libunwind-dev libfl-dev
+sudo apt install -y build-essential g++ automake git autoconf libtool libpcap-dev libpcre2-dev libpcre3-dev libnet1-dev zlib1g-dev luajit hwloc libdnet-dev libdumbnet-dev bison flex liblzma-dev openssl libssl-dev pkg-config libhwloc-dev cmake cpputest libsqlite3-dev uuid-dev libcmocka-dev libnetfilter-queue-dev libmnl-dev autotools-dev libluajit-5.1-dev libunwind-dev libfl-dev
 
 # LibDAQ
 git clone https://github.com/snort3/libdaq.git
